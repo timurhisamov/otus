2353f177710e3f54a1293cbdf1848c4f18c2e284
Хисамов Тимур <timur.hisamov@gmail.com>
2018-11-12 17:58:28 +0300

    l5: edit monitoring.sh

--- lesson5/monitoring.sh
+++ lesson5/monitoring.sh
@@ -29,23 +29,55 @@ if [ "$P1" = "-h" ]; then
             exit
 fi
 
+#Работа с датой последнего запуска скрипта . lastlaunch
+#	Формат 2018-11-12 с форматированием в вид 12/Nov/2018
 
-dt=`date '+%d-%m-%Y:%H-%M-%S'`
+#ll_d=`ls -alt --full-time output* | head -n 1 | awk '{print $6}'
+ll_d="2018-11-08"
+# Формат 17:56:19
+#ll_t=`ls -alt --full-time output* | head -n 1 | awk '{print $7}' | cut -f1 -d"."`
+ll_t="04:26:05"
+# Замена месяца. Форматирование в вид 12/Nov/2018
+ll_ac="`echo $ll_d | awk -F "-" '{print $3,$2,$1}' | sed 's/ /-/g' | {
+sed -e '
+s/-01-/\/Jan\//
+s/-02-/\/Feb\//
+s/-03-/\/Mar\//
+s/-04-/\/Apr\//
+s/-05-/\/May\//
+s/-06-/\/Jun\//
+s/-07-/\/Jul\//
+s/-08-/\/Aug\//
+s/-09-/\/Sep\//
+s/-10-/\/Oct\//
+s/-11-/\/Nov\//
+s/-12-/\/Dec\//'
+}`:$ll_t"
+
+#echo "ACCESS: $ll_ac"
+ll_er="`echo $ll_d | sed 's/-/\//g'` $ll_t"
+#echo "ERROR: $ll_er"
+
+dt=`date '+%d-%m-%Y_%H-%M-%S'`
 
 touch output_$dt.txt
 
 # X IP адресов (с наибольшим кол-вом запросов) с указанием кол-ва запросов c момента последнего запуска скрипта /var/log/nginx/access-4560-c8671a.log	
 echo "IP адресов (с наибольшим кол-вом запросов) с указанием кол-ва запросов c момента последнего запуска скрипта" > output_$dt.txt
-awk '{print $1}' access-4560-c8671a.log | sort | uniq -c | sort -rn | sed -n "1,${1}p" >> output_$dt.txt
+
+awk -v p="$ll_ac" '{if ($4 >= p) print $1}' access-4560-c8671a.log | sort | uniq -c | sort -rn | head -n $1 >> output_$dt.txt
 
 # Y запрашиваемых адресов (с наибольшим кол-вом запросов) с указанием кол-ва запросов c момента последнего запуска скрипта
 echo "запрашиваемых адресов (с наибольшим кол-вом запросов) с указанием кол-ва запросов c момента последнего запуска скрипта" >> output_$dt.txt
-awk '{print $7}' access-4560-c8671a.log | sort | uniq -c | sort -nr | head -n $2 >> output_$dt.txt
+awk -v p="$llac" '{if ($4 >= p) print $7}' access-4560-c8671a.log | sort | uniq -c | sort -nr | head -n $2 >> output_$dt.txt
 
 # все ошибки c момента последнего запуска
 
-echo "все ошибки c момента последнего запуска"
-awk '($9 !~ /200|304/)' access-4560-c8671a.log | awk '{print $9,$7,$11}' | sort | uniq -c | sort -r -n | head -n 70
+echo "все ошибки c момента последнего запуска" >> output_$dt.txt
+ll_d=`echo $ll_d | sed 's/-/\//g'`
+echo $ll_d
+echo $ll_t
+awk -v p="$ll_d" -v l="$ll_t" '{if (($1 >= p)&&($2 >= l)) print}' error-4560-d75c02.log >> output_$dt.txt
 
 cat output_$dt.txt
 #dt='ls -al lock | awk '{print $8}''
